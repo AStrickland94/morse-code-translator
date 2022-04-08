a8d5a9bd625a399b69f524c8d34284fa935b6386
Adam Strickland <adamstrickland94@gmail.com>
2022-04-08 15:29:53 +1000

    modularized and parametized functions, split translation directions into individual functions, added function testing, fixed issue with rémi char

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
-import index from "./modules/morseIndex.mjs";
+import index from "./modules/morseIndex.js";
+import engTrans from "./modules/engTrans.js";
+import morseTrans from "./modules/morseTrans.js";
 
 const engText = document.querySelector("#engText");
 const morseText = document.querySelector("#morseText");
@@ -11,40 +13,10 @@ morseText.value = "";
 engText.addEventListener("input", () => (active = "eng"));
 morseText.addEventListener("input", () => (active = "morse"));
 
-transBtn.addEventListener("click", () => translation());
-
-function translation() {
-    //if the most recent text was in the english box and it isn't empty
-    if (active === "eng" && engText.value !== "") {
-        morseText.value = engText.value
-            .toLowerCase()
-            .split("")
-            .map((char) => {
-                const found = Object.entries(index).find(
-                    (key) => key[0] === char,
-                );
-                //if the value isn't found then return an error character
-                if (found === undefined) {
-                    return "#";
-                }
-                return found[1];
-            })
-            .join(" ");
-        return;
-    } else if (active === "morse" && morseText.value !== "") {
-        engText.value = morseText.value
-            .split(" ")
-            .map((char) => {
-                const found = Object.entries(index).find(
-                    (key) => key[1] === char,
-                );
-                if (found === undefined) {
-                    return "#";
-                }
-                return found[0];
-            })
-            .join("");
-    } else {
-        console.log("No inputs detected");
+transBtn.addEventListener("click", () => {
+    if (active === "eng") {
+        morseText.value = engTrans(engText.value, index);
+    } else if (active === "morse") {
+        engText.value = morseTrans(morseText.value, index);
     }
-}
+});
